b2a9f2f631210dced8009ba68326f97da5e62e6f
Keïsha Alexander <90068272+K3A101@users.noreply.github.com>
2022-02-03 13:25:37 +0100

    Homepagina is responsive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log("Mijn Portfolio");
+
+var navigatieMenu = document.querySelector("header");
+var hamburgerMenu = document.querySelector("header img");
+
+console.log(navigatieMenu, hamburgerMenu);
+
+function maakZichtbaar (){
+    
+    navigatieMenu.classList.toggle("toon-menu");
+
+    if (navigatieMenu.classList.contains("toon-menu")) {
+       hamburgerMenu.classList.add("annuleren-knop");
+       
+    }
+
+    else hamburgerMenu.classList.remove("annuleren-knop");
+}
+
+hamburgerMenu.addEventListener("click", maakZichtbaar);
