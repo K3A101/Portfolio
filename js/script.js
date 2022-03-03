b3b00010a22d24c6e359ba7d1455c799c7d53de6
K3A101 <keisha.alexander@hva.nl>
2022-03-03 18:51:11 +0100

    Nieuwe afbeelding

--- js/script.js
+++ js/script.js
@@ -1,26 +1,20 @@
 console.log("Mijn Portfolio");
 
-var navigatieMenu = document.querySelector("header");
-var hamburgerMenu = document.querySelector("header img");
+let navigatieMenu = document.querySelector("header");
+let hamburgerMenu = document.querySelector("header img");
 
 console.log(navigatieMenu, hamburgerMenu);
 
-function maakZichtbaar (){
-    
+function maakZichtbaar() {
+
     navigatieMenu.classList.toggle("toon-menu");
 
-    if (navigatieMenu.classList.contains("toon-menu")) {
-       hamburgerMenu.classList.add("annuleren-knop");
-       
-    }
+    // if (navigatieMenu.classList.contains("toon-menu")) {
+    //    hamburgerMenu.classList.add("annuleren-knop");
 
-    else hamburgerMenu.classList.remove("annuleren-knop");
+    // }
+
+    // else hamburgerMenu.classList.remove("annuleren-knop");
 }
 
 hamburgerMenu.addEventListener("click", maakZichtbaar);
-
-
-
-
-
-
